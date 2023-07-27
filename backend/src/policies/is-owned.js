249c388cd8bbf177cf3ca70c0719097aee3789b8
ZwurK <etienne.routhier21@gmail.com>
2023-07-27 14:12:09 +0200

    [ADD] Delete widget

--- backend/src/policies/is-owned.js
+++ backend/src/policies/is-owned.js
@@ -4,9 +4,6 @@ module.exports = async (policyContext, config, { strapi }) => {
   
     const widgetId = policyContext.params.id;
 
-    console.log(user)
-    console.log(widgetId)
-
     const widget = await strapi.entityService.findOne('api::widget.widget', widgetId, {populate: { author: true }});
   
     if (!widget || !widget.author || widget.author.id !== user.id) {
--- frontend/pages/dashboard/widgets.vue
+++ frontend/pages/dashboard/widgets.vue
@@ -1,19 +1,35 @@
 <template>
     <div class="flex flex-wrap justify-center">
-        <WidgetCard v-for="widget in userWidgets" :key="widget.id" :title="widget.title"
-        :description="widget.description" :downloads="widget.downloads" />
+        <div v-for="widget in userWidgets" :key="widget.id">
+            <WidgetCard :title="widget.title" :description="widget.description" :downloads="widget.downloads" />
+            <button @click="handleDeleteWidget(widget.id)" class="bg-red-500">Delete</button>
+        </div>
     </div>
 </template>
 
 <script setup>
+
+// Display user's widgets
+
 const user = useStrapiUser();
-console.log(user)
 
 const userWidgets = computed(() => {
   return user.value ? user.value.widgets : [];
 });
 
+// Handle delete widget
+const { delete: _delete } = useStrapi()
+
+const handleDeleteWidget = async (id) => {
+  try {
+    const response = await _delete('widgets', id);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+};
+
 definePageMeta({
     layout: 'dashboard'
 });
+
 </script>
