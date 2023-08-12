931f8a3b7f71775af6667d3718b1775b377587e8
ZwurK <etienne.routhier21@gmail.com>
2023-08-12 19:50:44 +0200

    [ADD] Footer

---
+++ frontend/components/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <div
+      class="mt-10 flex flex-col md:flex-row md:justify-center items-center text-sm text-gray-500 md:gap-32"
+    >
+      <p class="order-2 md:order-1 mt-8 md:mt-0">
+        &copy;  Notion Workshop, 2023.
+      </p>
+      <div class="order-1 md:order-2">
+        <span class="px-2">About us</span>
+        <span class="px-2 border-l">Contact us</span>
+        <span class="px-2 border-l">Privacy Policy</span>
+      </div>
+    </div>
+</template>
